a9663e4f034216b645fb858a3f04640f4cd1f1b5
Mike Zhang <ubik00@yahoo.com>
2018-04-13 12:19:03 +0200

    added users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+      if user.save
+        render json: user
+      else
+        render json: user.errors
+      end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:full_name, :email, :password)
+  end
+end
