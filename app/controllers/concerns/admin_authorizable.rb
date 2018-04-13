b48b6f549f6316494f65c977a305af2cc2f96c73
Mike Zhang <ubik00@yahoo.com>
2018-04-13 12:26:39 +0200

    added admin_authorizable and token_authenticatable concerns

---
+++ app/controllers/concerns/admin_authorizable.rb
@@ -0,0 +1,15 @@
+class NotPermittedException < StandardError; end
+
+module AdminAuthorizable
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from NotPermittedException, with: -> { render json: { error: 'Not Permitted' }, status: :forbidden }
+  end
+
+  # :reek:ControlParameter
+  def authorize!(action)
+    raise NotPermittedException if action != :read && !current_user.admin?
+    true
+  end
+end
