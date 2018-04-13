9b8b7c4d62d019b10826858f4efcf2e1fbbe8da0
Mike Zhang <ubik00@yahoo.com>
2018-04-13 12:42:25 +0200

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+has_secure_password
+validates :full_name, presence: true
+VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+validates :email, presence: true,
+                  format: { with: VALID_EMAIL_REGEX },
+                  uniqueness: { case_sensitive: false }
+has_many :contracts, dependent: :destroy, autosave: true
+
 end
