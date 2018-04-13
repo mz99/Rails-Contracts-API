54c79827530c78333e92517913886d9886adb97b
Mike Zhang <ubik00@yahoo.com>
2018-04-13 12:25:19 +0200

    added authenticate_user, base, decode_authentication commands

---
+++ app/commands/base_command.rb
@@ -0,0 +1,33 @@
+class BaseCommand
+  attr_reader :result
+
+  def self.call(*args)
+    new(*args).call
+  end
+
+  def call
+    @result = nil
+    payload
+    self
+  end
+
+  def success?
+    errors.empty?
+  end
+
+  def errors
+    @errors ||= ActiveModel::Errors.new(self)
+  end
+
+  private
+
+  # :nocov:
+  def initialize(*_)
+    raise NotImplementedError, 'Implement `initialize` for classes inheriting from BaseCommand'
+  end
+
+  def payload
+    raise NotImplementedError, 'Implement `payload` for classes inheriting from BaseCommand'
+  end
+  # :nocov:
+end
