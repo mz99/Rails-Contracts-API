8f43b24cde5d84deb50832cf692d82117cb69b51
Mike Zhang <ubik00@yahoo.com>
2018-04-13 12:43:05 +0200

    added validations to contract model

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,2 +1,15 @@
 class Contract < ApplicationRecord
+  require 'date'
+  require 'time'
+  belongs_to :user, optional: true
+  validates :vendor, presence: true
+  validates :starts_on, presence: true
+  validates :ends_on, presence: true
+  validate  :ends_date_cannot_be_less_than_start_date
+
+  def ends_date_cannot_be_less_than_start_date
+    if ends_on < starts_on
+      errors.add(:ends_on, "Ends on date should be greater than start date")
+    end
+  end
 end
