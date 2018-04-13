ca02d6f85692661aa0ed1cb55c93782a46f24c05
Mike Zhang <ubik00@yahoo.com>
2018-04-13 12:27:49 +0200

    added includes to helpers for token and admin modules

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  include TokenAuthenticatable
+  include AdminAuthorizable
+  #protect_from_forgery with: :null_session
+
+  rescue_from ActiveRecord::RecordNotFound, with: -> { render json: { error: 'Not found' }, status: :not_found }
 end
