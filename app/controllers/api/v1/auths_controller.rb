c0f3ad6ecc9a58e3b9de44175ceb43498d849b4f
Mike Zhang <ubik00@yahoo.com>
2018-04-13 12:30:07 +0200

    added auths_controller

---
+++ app/controllers/api/v1/auths_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+    class AuthsController < ApplicationController
+      skip_before_action :authenticate_user
+
+      def create
+        token_command = AuthenticateUserCommand.call(*params.slice(:user, :password).values)
+
+        if token_command.success?
+          render json: { token: token_command.result }
+        else
+          render json: { error: token_command.errors }, status: :unauthorized
+        end
+      end
+    end
+  end
+end
